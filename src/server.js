f5656f8360a746513eeb67340ae9552024fdaf4a
Mateo <mateomfonseca@gmail.com>
2025-11-26 21:58:06 -0300

    Cargar LOGS para ver errores

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
+console.log("POSTGRES_PRISMA_URL:", process.env.POSTGRES_PRISMA_URL);
+console.log("POSTGRES_CONNECTION_STRING:", process.env.POSTGRES_CONNECTION_STRING);
+
+
 import app from "./app.js";
 import dotenv from "dotenv";
 
